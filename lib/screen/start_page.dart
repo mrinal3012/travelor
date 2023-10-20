60a5b3787a64ba0a3b0b706153bc8f7e90be34a4
https://github.com/mrinal3012 <mrinalpalit.cse@gmail.com>
2023-10-21 02:39:17 +0600

    add start page

--- lib/screen/start_page.dart
+++ lib/screen/start_page.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Startpage extends StatelessWidget {
   const Startpage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return SafeArea(
+      child: Scaffold(
+        body: Container(child: Column(
+          children: [
 
+            Expanded(flex: 3, child:Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Container(width: double.infinity,height: double.infinity,decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                image: DecorationImage(image: AssetImage("images/sea.jpg"),fit: BoxFit.cover)
+              ),),
+            )),
+            Expanded(flex: 2,child:SvgPicture.network("https://svgsilh.com/svg/2056977.svg")),
+
+          ],
+        ),)
+      ),
     );
   }
 }
