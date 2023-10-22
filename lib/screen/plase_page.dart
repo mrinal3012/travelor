3bdf56b9371517153e8d6aa1c65f59a6dc87a509
https://github.com/mrinal3012 <mrinalpalit.cse@gmail.com>
2023-10-22 21:44:46 +0600

    plase

---
+++ lib/screen/plase_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PlasePage extends StatefulWidget {
+  const PlasePage({super.key});
+
+  @override
+  State<PlasePage> createState() => _PlasePageState();
+}
+
+class _PlasePageState extends State<PlasePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
